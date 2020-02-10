0ea533387afe3a8181c19db1b719a28a1e08dbe9
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-10 16:54:04 -0300

    Create Class 5 || Cin.cpp

---
+++ Class 5 || Cin.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+
+    cout << "Enter a number!" << endl;
+    cin >> a;
+
+    cout << "Enter a number!" << endl;
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "The sum of those numbers is " << sum <<"!";
+
+
+    return 0;
+}
