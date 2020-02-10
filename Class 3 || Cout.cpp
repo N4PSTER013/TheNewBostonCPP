407ce7479ee1c1415e13091172b7aa14afc60fc8
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-10 16:15:38 -0300

    Create Class 3 || Cout.cpp

---
+++ Class 3 || Cout.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Boy, I love bacon ";
+    cout << "and ham!";
+
+    cout << "\n\nBoy, I love bacon " << endl;
+    cout << "and ham!" << endl;
+
+    cout << "\nBoy, I love bacon \n";
+    cout << "and ham!" << endl;
+
+    return 0;
+}
