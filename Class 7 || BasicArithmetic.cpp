ca6aefe31783467bc9395f1a7f91f8e020e3e8e0
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-10 16:53:29 -0300

    Create Class 7 || BasicArithmetic.cpp

---
+++ Class 7 || BasicArithmetic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int x = 6 + 1;
+    cout << "6 + 1 = " << x << endl;
+        x = 6 - 1;
+    cout << "6 - 1 = " << x << endl;
+        x = 6 * 2;
+    cout << "6 * 2 = " << x << endl;
+        x = 6 / 4;
+    cout << "6 / 4 = " <<x << endl;
+        x = 6 % 4;
+    cout << "The remainder of 6 / 4 is " << x << endl;
+        x = 6 + 1 * 9 * 7 + 4 - 10 / 5;
+    cout << "6 + 1 * 9 * 7 + 4 - 10 / 5 = " << x << endl;
+        x = 6 + 1 * 7 + (4 - 10) / 5;
+    cout << "6 + 1 * 7 + (4 - 10) / 5 = " << x << endl;
+
+
+    return 0;
+}
