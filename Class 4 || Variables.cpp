9abf8d0349af419b4edd41d8226fd88ece0cf9cb
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-10 16:21:20 -0300

    Create Class 4 || Variables.cpp

---
+++ Class 4 || Variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 4;
+    int b = 6;
+    int sum = a + b;
+
+    cout << a << " + " << b << " = " << sum;
+
+    return 0;
+}
