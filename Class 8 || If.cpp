ab0285f85d52dce384e042fd5834080dba30e8e8
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-10 17:00:50 -0300

    Create Class 8 || If.cpp

---
+++ Class 8 || If.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    if(5 > 2)
+    {
+        cout << "Bucky is awesome!" << endl;
+    }
+    if(1 > 2)
+    {
+        cout << "Bucky is awesome!" << endl;
+    }
+    if(2 >= 2)
+    {
+        cout << "Bucky is awesome!" << endl;
+    }
+    if(0 >= 2)
+    {
+        cout << "Bucky is awesome!" << endl;
+    }
+    if(5 != 2)
+    {
+        cout << "Bucky is awesome!" << endl;
+    }
+    if(3 == 3)
+    {
+        cout << "Bucky is awesome!" << endl;
+    }
+    int a = 100;
+    int b = 87;
+    if(a > b)
+    {
+        cout << "Bucky is awesome!" << endl;
+    }
+    return 0;
+}
