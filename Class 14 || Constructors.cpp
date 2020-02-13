f0d15d8d5443541f322a183608d01f3775f3cf7f
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-13 09:32:47 -0300

    Create Class 14 || Constructors.cpp

---
+++ Class 14 || Constructors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BuckysClass{
+    public:
+        BuckysClass(string z){
+            setName(z);
+        }
+        void setName(string x){
+            name = x;
+        }
+        string getName(){
+            return name;
+        }
+    private:
+        string name;
+};
+
+
+int main()
+{
+    BuckysClass bo("Lucky Bucky roberts");
+    cout << bo.getName();
+
+    BuckysClass bo2("Sally mcSalad");
+    cout << bo2.getName();
+    return 0;
+}
+© 2020 GitHub, Inc.
