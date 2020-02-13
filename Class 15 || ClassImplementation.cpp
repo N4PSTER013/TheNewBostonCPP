d15e74ed8dfa1718b8860dec10f74c30b459c6cc
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-13 10:11:46 -0300

    Update Class 15 || ClassImplementation.cpp

--- Class 15 || ClassImplementation.cpp
+++ Class 15 || ClassImplementation.cpp
@@ -1,9 +1,9 @@
-#include "Class15_Burrito.h"
+#include "Class15_Burrito.h" // insert the header file
 #include <iostream>
 
 using namespace std;
 
-Class15_Burrito::Class15_Burrito()
+Class15_Burrito::Class15_Burrito() // constructor
 {
 
     cout << "I am a banana! " << endl;
