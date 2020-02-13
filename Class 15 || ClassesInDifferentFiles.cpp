4a462a3a0303fa5d2d1458a4e76b9965e64d60f4
N4PSTER013 <56977028+N4PSTER013@users.noreply.github.com>
2020-02-13 09:53:25 -0300

    Create Class 15 || ClassesInDifferentFiles.cpp

---
+++ Class 15 || ClassesInDifferentFiles.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Class15_Burrito.h" // include the Burrito header file and Burrito class
+
+using namespace std;
+
+int main()
+{
+    Class15_Burrito bo; // create an object
+    return 0;
+}
